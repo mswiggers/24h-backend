65abe74f71d699668f59b1e083ab0cf041cbb387
bcallaerts <belian.callaerts@student.kuleuven.be>
2019-10-20 21:52:32 +0200

    Added export csv of the laps on request

--- competition/admin.py
+++ competition/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *
+import csv
+from django.http import HttpResponse
 
 
 admin.site.register(Group)
@@ -15,3 +17,21 @@ class RunnerAdmin(admin.ModelAdmin):
 @admin.register(Lap)
 class LapAdmin(admin.ModelAdmin):
     search_fields = ['runner__first_name', 'runner__last_name']
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+    
+    actions = ["export_as_csv"]
