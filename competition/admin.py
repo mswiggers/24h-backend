806e43277a11ccd54b61865b54f415ed31627e39
bcallaerts <belian.callaerts@student.kuleuven.be>
2019-10-20 21:34:49 +0200

    Added some admin search options

--- competition/admin.py
+++ competition/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Runner)
+
 admin.site.register(Group)
-admin.site.register(Lap)
 admin.site.register(HappyHour)
 admin.site.register(University)
 admin.site.register(GroupScore)
 admin.site.register(Criterium)
+
+@admin.register(Runner)
+class RunnerAdmin(admin.ModelAdmin):
+    search_fields = ['first_name', 'last_name']
+
+@admin.register(Lap)
+class LapAdmin(admin.ModelAdmin):
+    search_fields = ['runner__first_name', 'runner__last_name']
